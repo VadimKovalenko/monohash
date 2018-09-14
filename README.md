7c2edd6866e0ca2ee11d9015e42b92d374771c9a
Vadim Kovalenko <vadblaxmith@gmail.com>
2018-09-14 18:04:23 +0200

    Fixed bug with rowter

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# test-vue
+# monohash
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js'
 import VueMaterial from 'vue-material'
+import VueRouter from 'vue-router'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import App from './App'
@@ -9,6 +10,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
